3516a8e1f67f522d9b7d79c1321dee4e1637152a
Girish <girishkj009@gmail.com>
2021-02-25 19:27:38 +0530

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,37 @@
+const puzzleEl = document.querySelector('#puzzle');
+const guessEl = document.querySelector('#guess');
+let game1 
+
+
+
+window.addEventListener("keypress", function(e){
+    const guess = String.fromCharCode(e.charCode);
+    game1.makeGuess(guess);
+    renderGame();
+});
+
+
+const renderGame = () => {
+  puzzleEl.innerHTML = ""
+  guessEl.textContent = game1.statusMessage;
+  game1.Puzzle.split("").forEach((letter) => {
+    const letterEl = document.createElement('span');
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  })
+  
+
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle();
+  console.log(puzzle);
+  game1 = new Hangman(puzzle, 10);
+  renderGame();
+}
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+
+startGame()
 
